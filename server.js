68980af0d07c2d219a694994b2ab83a929b3e3bb
Adominguezes <alberto@adominguez.es>
2017-05-28 21:20:48 +0200

    update for heroku

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ var middleware= require('./middleware.js')
 
 app.use(middleware.logger);
 
-app.get('/about', middleware.requireAuthentication, function(request, response) {
-    response.send('about');
+app.get('/', middleware.requireAuthentication, function(request, response) {
+    response.render('/');
 });
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/demo'));
 
 app.listen(PORT, function() {
     console.log('Express Server started on port ' + PORT);
